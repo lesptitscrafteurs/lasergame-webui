f5694c6281d0e8855894ff960052f23457cfa4e9
VARQUET Arnaud <arnaud.varquet@gmail.com>
2022-06-28 10:39:46 +0200

    Ajout d'une fonction de chargement de l'application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,38 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { catchError, map, Observable, ObservableInput, of } from 'rxjs';
+import { ConfigService } from './services/config.service';
+
+function load (httpClient: HttpClient, config: ConfigService) : (() => Promise<boolean>) {
+  return () : Promise<boolean> => {
+    return new Promise<boolean>((resolve: (a: boolean) => void) : void => {
+      httpClient.get('./assets/config.json')
+        .pipe(
+          map((x: Object) => {
+            config.CONFIG = JSON.parse(JSON.stringify(x));
+            resolve(true);
+          }),
+          catchError((x: { status: number }, caught: Observable<void>): ObservableInput<{}> => {
+            if (x.status !== 404) {
+              console.error ("Fichier de configuration introuvable !");
+              resolve(false);
+            }
+            console.error ("Error lors du chargement du fichier de configuration ! Utilisation des valeurs par défaut.");
+            resolve(true);
+            return of({});
+          })
+        ).subscribe();
+    });
+  };
+}
+
 
 @NgModule({
   declarations: [
@@ -13,9 +41,20 @@ import { TooltipModule } from 'ngx-bootstrap/tooltip';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     TooltipModule.forRoot(),
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: load,
+      multi: true,
+      deps: [
+        HttpClient,
+        ConfigService,
+      ],
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/config.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+
+export class Config {
+  constructor (
+    public WS_URL : string = "ws://127.0.0.1:8000",
+  ) { }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfigService {
+  public CONFIG : Config = new Config ();
+
+  constructor() { }
+}
